efe304b4d350366f1aaa1da181ce295ddb9a8e3a
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-04-05 10:36:58 -0600

    Se ajusta el fronted segun requerido

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,13 +1,28 @@
 const Home = () => {
     return (
         <div>
-            <div class="row row-cols-1 row-cols-md-3" style={{padding:"10px"}}>
+            <div class="row row-cols-1 row-cols-md-3" style={{ padding: "10px" }}>
                 <div class="col mb-4">
                     <div class="card">
-                    <img src="https://media.istockphoto.com/id/1303583671/es/foto/taza-de-caf%C3%A9-con-humo-y-granos-de-caf%C3%A9-sobre-fondo-de-madera-viejo.jpg?s=2048x2048&w=is&k=20&c=PjxTESE6A1AkZ8Yq0omzyfhHMU3t8JuFOPQ_bt5FW4Y=" class="card-img-top" alt="https://media.istockphoto.com/id/1303583671/es/foto/taza-de-caf%C3%A9-con-humo-y-granos-de-caf%C3%A9-sobre-fondo-de-madera-viejo.jpg?s=2048x2048&w=is&k=20&c=PjxTESE6A1AkZ8Yq0omzyfhHMU3t8JuFOPQ_bt5FW4Y="></img>
+                        <img src="https://media.istockphoto.com/id/1303583671/es/foto/taza-de-caf%C3%A9-con-humo-y-granos-de-caf%C3%A9-sobre-fondo-de-madera-viejo.jpg?s=2048x2048&w=is&k=20&c=PjxTESE6A1AkZ8Yq0omzyfhHMU3t8JuFOPQ_bt5FW4Y=" class="card-img-top" alt="https://media.istockphoto.com/id/1303583671/es/foto/taza-de-caf%C3%A9-con-humo-y-granos-de-caf%C3%A9-sobre-fondo-de-madera-viejo.jpg?s=2048x2048&w=is&k=20&c=PjxTESE6A1AkZ8Yq0omzyfhHMU3t8JuFOPQ_bt5FW4Y="></img>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <div class="contenedor-horizontal">
+                                <p class="contenido-horizontal texto-time">3min ago</p>
+                                <p class="contenido-horizontal"><button type="button" class="btn btn-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
+                                        <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z" />
+                                    </svg> 43k</button></p>
+                            </div>
+                            <p class="account-letter">@eric</p>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="comments-letter">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="23" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16">
+                                    <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z" />
+                                </svg>
+                                 Comments (15)
+
+
+                            </p>
                         </div>
                     </div>
                 </div>
