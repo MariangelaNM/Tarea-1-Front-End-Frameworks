a5c6ddc2e3277e164a7e868dbb76a3c29be88ab0
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-04-05 09:38:39 -0600

    Se ajusta navBar y se crean cards para imprimir data

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,7 +1,44 @@
 const Home = () => {
-    return(
+    return (
         <div>
-            <h1>View Home</h1>
+            <div class="row row-cols-1 row-cols-md-3" style={{padding:"10px"}}>
+                <div class="col mb-4">
+                    <div class="card">
+                    <img src="https://media.istockphoto.com/id/1303583671/es/foto/taza-de-caf%C3%A9-con-humo-y-granos-de-caf%C3%A9-sobre-fondo-de-madera-viejo.jpg?s=2048x2048&w=is&k=20&c=PjxTESE6A1AkZ8Yq0omzyfhHMU3t8JuFOPQ_bt5FW4Y=" class="card-img-top" alt="https://media.istockphoto.com/id/1303583671/es/foto/taza-de-caf%C3%A9-con-humo-y-granos-de-caf%C3%A9-sobre-fondo-de-madera-viejo.jpg?s=2048x2048&w=is&k=20&c=PjxTESE6A1AkZ8Yq0omzyfhHMU3t8JuFOPQ_bt5FW4Y="></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
