ed589167489836ec8ac81fdfc088cd2d140439a5
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-04-08 16:29:54 -0600

    se añde popup

--- my-app/src/layouts/navbar.js
+++ my-app/src/layouts/navbar.js
@@ -1,7 +1,11 @@
 import { Navbar, Nav, Container } from "react-bootstrap"
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom"
-
+import Modal from "../components/modal"
+import profile from "../components/data/profile.json"
 const NavBarExample = () => {
+
+    const [show, setShow] = useState(false);
     return (
         <>
             <Navbar className="navBg" variant="dark" expand="lg">
@@ -13,18 +17,26 @@ const NavBarExample = () => {
                     </Navbar.Brand>
 
                     <Navbar.Toggle>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16"  onClick={() => setShow(true)}>
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                         </svg>
                     </Navbar.Toggle>
-
+                   
+                    <Modal title="My Modal" onClose={() => setShow(false)} show={show}>
+                        <div>
+                        <img class="circular--square"src={profile.img}></img>
+                              
+                        </div>
+                        <p>This is modal body</p>
+                    </Modal>
                 </Container>
             </Navbar>
 
             <section>
                 <Outlet></Outlet>
             </section>
+
         </>
     )
 }
