933aa4f7b3bd0e3e3b49e48aee6002b56e92e1d2
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-04-21 20:27:55 -0600

    Se lee de la api la data del perfil

--- my-app/src/components/modal.js
+++ my-app/src/components/modal.js
@@ -1,31 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./modal.css";
-import profile from "./data/profile.json"
-import Loggin from "./loggin/loggin";
 
 function Modal({ setOpenModal }) {
+  const [result, setResult] = useState(undefined);
+
+  useEffect(() => {
+    callDataProfile();
+  }, []);
+
+  function callDataProfile() {
+    const token = JSON.parse(localStorage.getItem("token")).token;
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    debugger;
+    fetch("https://three-points.herokuapp.com/api//users/me", requestOptions)
+      .then(async (response) => {
+        const isJson = response.headers
+          .get("content-type")
+          ?.includes("application/json");
+        const data = isJson && (await response.json());
+        // check for error response
+        if (!response.ok) {
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+        setResult(data);
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error.toString() });
+        console.error("There was an error!", error);
+      });
+  }
+
   return (
     <div className="modalBackground">
-      <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            X
-          </button>
-        </div>
-        <img className="circular--square" style={{ width: "250px" }} src={profile.img}></img>
+      {result != undefined && (
+        <div className="modalContainer">
+          <div className="titleCloseBtn">
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+            >
+              X
+            </button>
+          </div>
+          <img
+            className="circular--square"
+            style={{ width: "250px" }}
+            src={result.avatar}
+          ></img>
 
-        <div className="user">
-          <p>@{profile.user}</p>
-
+          <div className="user">
+            <p>@{result.name}</p>
+          </div>
+          <div className="body">
+            <p>{result.bio}</p>
+          </div>
         </div>
-        <div className="body">
-          <p>{profile.description}</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
